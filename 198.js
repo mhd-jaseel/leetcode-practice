cf7075fdbb80190c6ca61852f6f0b5a83ad11f9d
mhd-jaseel <mohammejaseel90@gmail.com>
2026-07-22 21:44:43 +0530

    198.js

---
+++ 198.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    let newArr=[]
+     for (let i=0;i<nums.length-2;i++){
+        newArr.push(nums[i]+nums[i+2])
+     }
+     console.log(newArr)
+};
+
+rob([1,4,2])
