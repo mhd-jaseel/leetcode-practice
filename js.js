ee1f007ced4e4ad3c9678ebdd6307cef09a3306a
mhd-jaseel <mohammejaseel90@gmail.com>
2026-07-28 22:28:19 +0530

    ??

--- js.js
+++ js.js
@@ -1,10 +1,21 @@
-var n =2;
-function square(num){
-    var ans =num*num;
-    return ans
+// var n =2;
+// function square(num){
+//     var ans =num*num;
+//     return ans
 
-}
-var square2 =square(n)
-var square4=square(4)
-console.log(square2,square4)
-console.log("heello")
+// }
+// var square2 =square(n)
+// var square4=square(4)
+// console.log(square2,square4)
+// console.log("heello")
+let age;
+console.log(age)
+let x= 5
+console.log(--x)
+
+// console.log(++x)
+// console.log(x++)
+// console.log(x--)
+let value = 0;
+
+console.log(value ?? 100);
