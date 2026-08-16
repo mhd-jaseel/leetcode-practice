33e288d1186b874ce306b878ae4934e89e83637e
mhd-jaseel <mohammejaseel90@gmail.com>
2026-08-16 22:23:08 +0530

    just push

---
+++ 88.js
@@ -0,0 +1,11 @@
+let nums1 = []
+var a = 3
+var nums2 = [2,5,6]
+ var n = 3
+
+// var merge = function(nums1, m, nums2, n) {
+//     let arr =nums1.concat(m,nums2,n)
+// };
+
+let arr =nums1.concat(nums2)
+console.log("hello ")
