6b0fbcf014b0ec8059ad8530b344157a4c4c5769
mhd-jaseel <mohammejaseel90@gmail.com>
2026-08-27 21:55:31 +0530

    fix: remove frontend serving from backend

--- jsut.js
+++ jsut.js
@@ -1,7 +1,8 @@
-let a = "jaseel"
-let rev=""
-for(let i = a.length-1 ;i>=0;i--){
-  rev+=a[i]
-}
-console.log(rev)
+// let a = "jaseel"
+// let rev=""
+// for(let i = a.length-1 ;i>=0;i--){
+//   rev+=a[i]
+// }
+// console.log(rev)
 
+console.log("jaseel"-100)
