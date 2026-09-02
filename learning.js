14c96dd4dff3f006c76856ee0f911660106e7b90
mhd-jaseel <mohammejaseel90@gmail.com>
2026-09-02 21:24:58 +0530

    learning process..

---
+++ learning.js
@@ -0,0 +1,49 @@
+// var n =2;
+// function square(num){
+//     var ans =num*num;
+//     return ans
+
+// }
+// var square2 =square(n)
+// var square4=square(4)
+// console.log(square2,square4)
+// console.log("heello")
+let age;
+console.log(age)
+let x= 5
+console.log(--x)
+
+console.log(++x)
+console.log(x++)
+console.log(x--)
+let value = 0;
+
+console.log(value ?? 100);
+
+
+// function greet (){
+//     console.log("hello")
+// }
+// function callback(fn){
+//     fn()
+// }
+
+// callback(greet)
+
+
+(function (){
+    console.log("hai")
+})()
+
+//pure
+function add(a,b){
+    return a+b
+}
+console.log(add(4,5))
+ //impure
+
+ let value1 =20 
+ function dub(){
+   console.log(value1*2)
+ }
+ dub()
