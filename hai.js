8adc8987a9ea968439b4f247022d68a47977006c
mhd-jaseel <mohammejaseel90@gmail.com>
2026-09-05 20:58:23 +0530

    learning process..

--- hai.js
+++ hai.js
@@ -1,40 +1,9 @@
-// let n=10203004
+function reverse(n){
+    let rev=''
+    for(i=n.length-1;i>=0;i--){
+      rev+=n[i]
 
-
-// var sumAndMultiply = function(n) {
-//     var input = n.toString()
-//     var x=[]
-//    for(let i =0;i<input.length;i++){
-      
-//      if (input[i]!="0") {
-//         x.push(Number(input[i]))
-//      } 
-//    } 
-//   let sum =x.reduce((a,b)=>a+b,0)
-//   let num =Number(x.join(""))
-//   return sum*num
-// };
-
-// console.log(sumAndMultiply(10203004))
-
-
-var sumAndMultiply = function(n) {
-    let arr =[]
-    let  num=n.toString().split("")
-    let sum =num.reduce((acc,curr)=>acc+Number(curr),0)
-    return sum
-};
-console.log(sumAndMultiply(12345))
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
+    return rev
+}
+console.log(reverse("jaseel"))
